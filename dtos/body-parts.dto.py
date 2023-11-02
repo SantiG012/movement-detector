35475988ac8bfffa355ebc206644ec0e4282b428
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-11-02 12:22:41 -0500

    Add objects

---
+++ dtos/body-parts.dto.py
@@ -0,0 +1,14 @@
+from ..clases.point import Point
+
+class BodyPartsDto:
+    left_wrist: Point
+    right_wrist: Point
+    left_elbow: Point
+    right_elbow: Point
+    left_twist: Point
+    right_twist: Point
+
+    def __init__(self) -> None:
+        pass
+
+    
