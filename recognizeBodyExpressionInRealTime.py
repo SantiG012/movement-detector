5384f9bf3630fa369fcfeecb64d7639b00635da1
hacksJuanDavid <josuacoty000@gmail.com>
2023-11-02 17:05:35 -0500

    Update send 1 menssage HandsUp and 1 send menssage HandsDown

--- recognizeBodyExpressionInRealTime.py
+++ recognizeBodyExpressionInRealTime.py
@@ -6,8 +6,10 @@ import json
 from landMark import LandMark
 
 # Variables hands target
-handsUp = 0
-handsDown = 0
+handsUp = False
+handsDown = False
+handsUpSent = False
+handsDownSent = False
 
 # Method pulsar producer
 def pulsarProducer():
@@ -20,16 +22,20 @@ def pulsarSendMessage(producer, handsSendMessage):
     producer.send(('Hands ' + handsSendMessage).encode('utf-8'))
     producer.flush()
 
+# Filter landmarks body
 def filter(landmarks)->list:
     return [landmarks[landmark_index] for landmark_index in BODY_LANDMARKS.values()]
 
+# Instantiate landmarks
 def instantiate(landmarks)->list[LandMark]:
     return [LandMark(landmark.x, landmark.y, landmark.z) for landmark in landmarks]
 
+# Send landmarks
 def send(landmarks,producer)->None:
     producer.send((landmarks).encode('utf-8'))
     producer.flush()
 
+# Landmark to json
 def landmarkToJson(landmark:LandMark)->str:
     return {
         "x": landmark.x,
@@ -37,12 +43,12 @@ def landmarkToJson(landmark:LandMark)->str:
         "z": landmark.z
     }
 
+# Prepare landmarks
 def prepare(landmarks):
     landmarks = filter(landmarks)
     landmarks = instantiate(landmarks)
     landmarks = json.dumps(landmarks,default=landmarkToJson)
     return landmarks
-
 
 # Mediapipe utilities
 def recognizeBodyExpressionInRealTime():
@@ -64,7 +70,7 @@ def recognizeBodyExpressionInRealTime():
             if not ret:
                 break
             
-            frame = cv2.resize(frame, (1600, 900)) # Resize the frame
+            frame = cv2.resize(frame, (1000, 1000)) # Resize the frame
             frameRgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Convert the frame to RGB
             results = pose.process(frameRgb) # Process the frame
 
@@ -74,45 +80,73 @@ def recognizeBodyExpressionInRealTime():
                                         mpDrawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
                                         mpDrawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
                                         )
-                
+
                 # Get the landmarks for the wrists (left and right)
                 leftWrist = results.pose_landmarks.landmark[15]
                 rightWrist = results.pose_landmarks.landmark[16]
 
                 # Check if both wrists are above a certain threshold (adjust as needed)
                 if leftWrist.y < 0.2 and rightWrist.y < 0.2:
+                    # Landmarks to json
                     landmarks = prepare(results.pose_landmarks.landmark)
-                    send(landmarks,pulsarProducer())
 
-                    # Hands are up, so draw a bounding box and message
-                    cv2.rectangle(frame, (50, 50), (590, 150), (0, 255, 0), 2)
-                    cv2.putText(frame, "Hands Up!", (60, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                    # Send message pulsar
-                    handsUp = 1
-                    if handsUp == 1:
-                        pulsarSendMessage(pulsarProducer(), 'Up')
-                        #print('SendUp')
-                        handsUp = 0
+                    if not handsUpSent:
+                        # Hands are up, so draw a bounding box and message
+                        cv2.rectangle(frame, (50, 50), (590, 150), (0, 255, 0), 2)
+                        cv2.putText(frame, "Hands Up!", (60, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+                        # Activate handsUp
+                        handsUp = True
+
+                        # Send message pulsar HandsUp
+                        if handsUp:
+                            # Set handsUpSent to True
+                            handsUpSent = True
+                            # Send menssage pulsar
+                            pulsarSendMessage(pulsarProducer(), "HandsUp")
+                            
+                            # Send landmarks
+                            send(landmarks,pulsarProducer())
+                            
+                            # Set handsDownSent to False
+                            handsDownSent = False
 
                 # Get the landmarks for the hands down (left and right)
                 left_hip = results.pose_landmarks.landmark[15]
                 right_hip = results.pose_landmarks.landmark[16]
 
                 # Check if both hands are below a certain threshold (adjust as needed)
-                if left_hip.y > 0.8 and right_hip.y > 0.8 is not None and results.pose_landmarks.landmark[12].y < 0.5:
+                if left_hip.y > 0.8 and right_hip.y is not None and right_hip.y > 0.8 and results.pose_landmarks.landmark[12].y < 0.5:
+                    # Landmarks to json
                     landmarks = prepare(results.pose_landmarks.landmark)
-                    send(landmarks,pulsarProducer())
                     
-                    # Hands are up, so draw a bounding box and message
-                    cv2.rectangle(frame, (50, 50), (590, 150), (0, 0, 255), 2)
-                    cv2.putText(frame, "Hands Down!", (60, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                    # Send message pulsar
-                    handsDown = 1
-                    if handsDown == 1:
-                        pulsarSendMessage(pulsarProducer(), 'Down')
-                        #print('SendDown')
-                        handsDown = 0
-                    
+                    if not handsDownSent:
+                        # Hands are up, so draw a bounding box and message
+                        cv2.rectangle(frame, (50, 50), (590, 150), (0, 0, 255), 2)
+                        cv2.putText(frame, "Hands Down!", (60, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+                        # Activate handsDown
+                        handsDown = True
+
+                        # Send message pulsar HandsDown
+                        if handsDown:
+                            # Set handsDownSent to True
+                            handsDownSent = True
+                            
+                            # Send menssage pulsar
+                            pulsarSendMessage(pulsarProducer(), "HandsDown")
+
+                            # Send landmarks
+                            send(landmarks,pulsarProducer())
+
+                            # Set handsUpSent to False
+                            handsUpSent = False
+                
+                # Reset handsUpSent and handsDownSent when hands change position to avoid spamming
+                if (leftWrist.y >= 0.2 or rightWrist.y >= 0.2) and (left_hip.y <= 0.8 or right_hip.y is None or right_hip.y <= 0.8 or results.pose_landmarks.landmark[12].y >= 0.5):
+                    handsUpSent = False
+                    handsDownSent = False
+
             # Show the frame
             cv2.imshow('Mediapipe Feed', frame)
             # Break if q is pressed
